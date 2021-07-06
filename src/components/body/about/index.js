73a1f08d6b859e0dc01455d2ef1a8cc36b2a395d
raynamecham <rayna.mecham@gmail.com>
2021-07-06 14:51:47 -0600

    added body components

---
+++ src/components/body/about/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './about.css'
+
+function About() {
+    return (
+        <div className="about">
+            <div className="about-top">
+                <div className="about-info">
+                    Hello there 👋! I'm Rayna Mecham. I'm a frontend software/web developer. 
+                </div>
+                <div className="about-photo">
+
+                </div>
+            </div>
+            <div className="about-bottom">
+                This is contact
+            </div>
+        </div>
+    )
+}
+
+export default About
--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import './mobile.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { faSplotch } from '@fortawesome/free-solid-svg-icons'
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
-function Mobile() {
+function Mobile({isOpen, setIsOpen}) {
     return (
-        <div>
-            This is mobile
+        <div className="mobile">
+            <div className="close-icon" onClick={()=>setIsOpen(!isOpen)}>
+                <FontAwesomeIcon icon={faTimesCircle} style={{ display: "flex", justifyContent: "flex-end", fontSize: "24px" }} />
+            </div>
+            <div className="mobile-options">
+            <div className="mobile-option">
+                <a href='#project'>
+                    <FontAwesomeIcon icon={faSplotch} style={{ display: "flex", alignItems: "center", marginRight: "4px" }} />Projects
+                </a>
+            </div>
+            <div className="mobile-option">
+                <a href='#skills'>
+                    <FontAwesomeIcon icon={faLaptopCode} style={{ display: "flex", alignItems: "center", marginRight: "4px" }} />Skills
+                </a>
+            </div>
+            <div className="mobile-option">
+                <a href="#work">
+                    <FontAwesomeIcon icon={faBriefcase} style={{ display: "flex", alignItems: "center", marginRight: "4px" }} />Work
+                </a>
+            </div>
+            <div className="mobile-option">
+                <a href="#contact">
+                    <FontAwesomeIcon icon={faUserCircle} style={{ display: "flex", alignItems: "center", marginRight: "4px" }} />Contact
+                </a>
+            </div>
+            </div>
         </div>
     )
 }
