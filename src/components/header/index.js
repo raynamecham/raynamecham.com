d1e864583688d5c367f5bd0ed111ba14b56e2e6e
raynamecham <rayna.mecham@gmail.com>
2021-07-06 13:31:28 -0600

    icons in place

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import "./header.css";
 import Web from "./web/index";
 import Mobile from "./mobile/index";
 
 function Header() {
+  const [isOpen, setIsOpen]= useState(false);
   return (
     <div className="header">
       <div className="logo">Rayna Mecham</div>
       <div className="menu">
         <div className="web-menu"><Web /></div>
-        <div className="mobile-menu"><Mobile /></div>
+        <div className="mobile-menu">
+          <div onClick={()=>setIsOpen(!isOpen)}>
+            <FontAwesomeIcon icon={faBars} style={{ fontSize: "24px" }} />
+          </div>
+          {isOpen && <Mobile isOpen={isOpen} setIsOpen={setIsOpen} />}
+        </div>
       </div>
     </div>
   );
--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './web.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSplotch } from '@fortawesome/free-solid-svg-icons'
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 function Web() {
     return (
-        <div>
-            This is web view
+        <div className="web">
+            <div className="web-option">
+                <a href='#project'>
+                    <FontAwesomeIcon icon={faSplotch} />Projects
+                </a>
+            </div>
+            <div className="web-option">
+                <a href='#skills'>
+                    <FontAwesomeIcon icon={faLaptopCode} />Skills
+                </a>
+            </div>
+            <div className="web-option">
+                <a href="#work">
+                    <FontAwesomeIcon icon={faBriefcase} />Work
+                </a>
+            </div>
+            <div className="web-option">
+                <a href="#contact">
+                    <FontAwesomeIcon icon={faUserCircle} />Contact
+                </a>
+            </div>
         </div>
     )
 }
