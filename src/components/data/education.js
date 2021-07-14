ddf2ba32e3df1a2eacae2c1271d5453378675454
raynamecham <rayna.mecham@gmail.com>
2021-07-13 18:48:22 -0600

    css changes, updated resume

--- src/components/data/education.js
+++ src/components/data/education.js
@@ -5,13 +5,13 @@ export const EducationData = [
     dateJoined: "January",
     dateEnd: "July 2021",
     companyLogo: require("../../assets/logos/hackbright.png").default,
-    work: "Hackbright is a training program designed to help women become amazing programmers. It teaches the fundamentals of modern web development, including front-end, back-end, and database programming.",
+    work: "Hackbright is a coding bootcamp designed to help women become amazing programmers. It teaches the fundamentals of modern web development, including frontend, backend, and database programming.",
   },
   {
     company: "Weber State University",
     dateJoined: "2011",
     dateEnd: "2013",
     companyLogo: require("../../assets/logos/weber.png").default,
-    work: "Associate of Science Degree - General Studies",
+    work: "Associate of Science - General Studies",
   },
 ];
