28cd4f999077b72a4b2f2aafefdb7acb3e5c262d
raynamecham <rayna.mecham@gmail.com>
2021-09-11 19:30:45 -0600

    updated about

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -1,29 +1,29 @@
-import React from "react";
-import "./about.css";
-import SocialContact from "../../common/social-contact/index";
+import React from 'react';
+import './about.css';
+import SocialContact from '../../common/social-contact/index';
 
 function About() {
-  return (
-    <div className="about">
-      <div className="about-top">
-        <div className="about-info">
-          Hello there 👋, I'm
-          <br /> <span className="info-name">Rayna Mecham</span>.
-          <br />
-          Software engineer. RPG conqueror. Avid reader.{" "}
-          <span className="no-break">TV enthusiast.</span>
-        </div>
-        <div className="about-photo">
-          <img
-            src={require("../../../assets/coding.png").default}
-            alt="coding girl"
-            className="picture"
-          />
-        </div>
-      </div>
-      <SocialContact />
-    </div>
-  );
+	return (
+		<div className="about">
+			<div className="about-top">
+				<div className="about-info">
+					Hello there 👋, I'm
+					<br /> <span className="info-name">Rayna Mecham</span>.
+					<br />
+					Web/Software Developer. RPG conqueror. Avid reader.{' '}
+					<span className="no-break">TV enthusiast.</span>
+				</div>
+				<div className="about-photo">
+					<img
+						src={require('../../../assets/coding.png').default}
+						alt="coding girl"
+						className="picture"
+					/>
+				</div>
+			</div>
+			<SocialContact />
+		</div>
+	);
 }
 
 export default About;
